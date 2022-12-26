15891a3ee88a05bfa1eb4430755e85d1efc90a1e
Suresh <sureshchoudharytrue@gmail.com>
2022-12-26 21:32:38 +0530

    implementing single linked list

--- Linked List/singleLinkedList.py
+++ Linked List/singleLinkedList.py
@@ -1,1 +1,81 @@
-print("hello world")
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data, self.head)
+        self.head = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def insert_after_node(self, prev_node, data):
+        if not prev_node:
+            print("Previous node is not in the list")
+            return
+        new_node = Node(data, prev_node.next)
+        prev_node.next = new_node
+
+    def delete_node(self, key):
+        current = self.head
+        if current and current.data == key:
+            self.head = current.next
+            current = None
+            return
+        prev = None
+        while current and current.data != key:
+            prev = current
+            current = current.next
+        if current is None:
+            return
+        prev.next = current.next
+        current = None
+
+    def delete_node_at_pos(self, pos):
+        current = self.head
+        if pos == 0:
+            self.head = current.next
+            current = None
+            return
+        prev = None
+        count = 0
+        while current and count != pos:
+            prev = current
+            current = current.next
+            count += 1
+        if current is None:
+            return
+        prev.next = current.next
+        current = None
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+
+
+# Create a single linked list
+linked_list = LinkedList()
+
+# Add nodes to the single linked list
+linked_list.insert_at_end(1)
+linked_list.insert_at_end(2)
+linked_list.insert_at_end(3)
+linked_list.insert_at_end(4)
+
+#print the single linked list
+linked_list.print_list()
+
